ca493c24457f6022027a999bb306c4af0ee4238e
Leo Brad <>
2024-03-04 14:40:36 +0800

    [Add] Add main file export.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import HighLight from '~/class/HighLight';
-import JavascriptLexer from '~/class/JavascriptLexer';
-
-const highLight = new HighLight();
-highLight.addLexer(JavascriptLexer);
-console.log(highLight.parse('13232ace'));
+export * as Lexer from '~/class/Lexer';
+export * as HighLight from '~/class/HighLight';
+export * as JavascriptLexer from '~/class/JavascriptLexer';
+export * as LocationLexer from '~/class/LocationLexer';
+export * as ShellLexer from '~/class/ShellLexer';
+export * as VersionLexer from '~/class/VersionLexer';
+export * as YamlLexer from '~/class/VersionLexer';

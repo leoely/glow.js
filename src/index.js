950382e8c88010d34ef42d0fb0b417e5284470cd
Leo Brad <>
2024-03-20 10:33:52 +0800

    [Publish] Publish v0.0.4.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
-import HighLight from '~/class/HighLight';
-import CtfLexer from '~/class/CtfLexer';
-
-//export * as Lexer from '~/class/Lexer';
-//export * as HighLight from '~/class/HighLight';
-//export * as JavascriptLexer from '~/class/JavascriptLexer';
-//export * as LocationLexer from '~/class/LocationLexer';
-//export * as ShellLexer from '~/class/ShellLexer';
-//export * as VersionLexer from '~/class/VersionLexer';
-//export * as YamlLexer from '~/class/VersionLexer';
-
-const highLight = new HighLight();
-highLight.addLexer(CtfLexer);
-console.log(highLight.parse(`
-  [+] bold, gray, underline, dim:
-    | This is example text is explain ctf,
-    | "+" that is mean add a new text or passage.
-    | bold, red and so on is text or passage formate.
-`));
+export * as Lexer from '~/class/Lexer';
+export * as HighLight from '~/class/HighLight';
+export * as JavascriptLexer from '~/class/JavascriptLexer';
+export * as LocationLexer from '~/class/LocationLexer';
+export * as ShellLexer from '~/class/ShellLexer';
+export * as VersionLexer from '~/class/VersionLexer';
+export * as YamlLexer from '~/class/VersionLexer';
+export * as CtfLexer from '~/class/CtfLexer';

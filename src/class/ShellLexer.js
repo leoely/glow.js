2906f3a9bbb4f6a42fc8b0ab1b654f3c8a3d1017
Leo Brad <>
2024-04-07 12:52:42 +0800

    [Change] Change ShellLexer parse.

--- src/class/ShellLexer.js
+++ src/class/ShellLexer.js
@@ -9,54 +9,119 @@ class ShellLexer extends Lexer {
     switch (this.status) {
       case 0:
         switch (char) {
-          case '-':
-            this.ans.push(this.makeLexer('centerLine', '-'));
+          case '|':
+            this.ans.push(this.makeLexer('or', '|'));
+            return this.quit();
+          case '&':
+            this.ans.push(this.makeLexer('and', '&'));
+            return this.quit();
+          case '(':
+            this.ans.push(this.makeLexer('bracket', '('));
+            return this.quit();
+          case ')':
+            this.ans.push(this.makeLexer('bracket', ')'));
+            return this.quit();
+          case '<':
+            this.ans.push(this.makeLexer('angleBracket', '<'));
+            return this.quit();
+          case '>':
+            this.ans.push(this.makeLexer('angleBracket', '>'));
+            return this.quit();
+          case '{':
+            this.ans.push(this.makeLexer('bigBracket', '{'));
+            return this.quit();
+          case '}':
+            this.ans.push(this.makeLexer('bigBracket', '}'));
             return this.quit();
           case '|':
-            this.status = 1;
+            this.ans.push(this.makeLexer('diving', '|'));
+            return this.quit();
+          case '*':
+            this.ans.push(this.makeLexer('asterisk', '*'));
+            return this.quit();
+          case '!':
+            this.ans.push(this.makeLexer('exclamation', '!'));
+            return this.quit();
+          case '?':
+            this.ans.push(this.makeLexer('questionMark', '?'));
+            return this.quit();
+          case '@':
+            this.ans.push(this.makeLexer('questionMark', '?'));
+            return this.quit();
+          case ':':
+            this.elems = [];
+            this.elems.push(char);
+            this.status = 11;
+          case '-':
+            this.elems = [];
+            this.elems.push(char);
+            this.status = 9;
             return;
           case '#':
             this.elems = [];
             this.elems.push(char);
-            this.status = 2;
+            this.status = 1;
+          case '"':
+            this.elems = [];
+            this.elems.push(char);
+            this.status = 5;
+            return;
+          case "'":
+            this.elems = [];
+            this.elems.push(char);
+            this.status = 6;
+            return;
+          case "`":
+            this.elems = [];
+            this.elems.push(char);
+            this.status = 7;
+            return;
+          case '$':
+            this.elems = [];
+            this.elems.push(char);
+            this.status = 8;
             return;
         }
         const code = char.charCodeAt(0);
         if ((code >= 97 && code <= 122) ||
           (code >= 59 && code <= 64) || (code >= 33 && code <= 42) ||
-          (code >= 47 && code <= 47) || (code >= 123 && code <= 153)) {
+          (code >= 47 && code <= 57) || (code >= 123 && code <= 153)) {
           this.elems = [];
           this.elems.push(char);
-          this.status = 4;
+          this.status = 3;
           return;
         }
         break;
-      case 1:
-        switch (char) {
-          case ' ':
-            this.ans.push(this.makeLexer('symbol', '|'));
-            return this.quit();
-          case 'EOF':
-            return this.quit();
-          default:
-            return this.quit();
-        }
-        break;
-      case 2: {
+      case 1: {
         if (char === '!') {
           this.elems.push(char);
-          this.status = 3;
+          this.status = 2;
         } else {
           return this.quit();
         }
         break;
       }
-      case 3: {
+      case 2: {
         if (char === '\n' || char === 'EOF') {
           this.ans.push(this.makeLexer('hashbangComment', this.elems.join('')));
           return this.quit();
         } else {
           this.elems.push(char);
+        }
+        break;
+      }
+      case 3: {
+        const code = char.charCodeAt(0);
+        if ((code >= 97 && code <= 122) ||
+          (code >= 59 && code <= 64) || (code >= 33 && code <= 42) ||
+          (code >= 47 && code <= 57) || (code >= 123 && code <= 153)) {
+          this.elems.push(char);
+        } else if (char === '.') {
+          this.elems.push(char);
+          this.status = 4;
+        } else {
+          this.ans.push(this.makeLexer('command', this.elems.join('')));
+          return this.quit();
         }
         break;
       }
@@ -66,23 +131,86 @@ class ShellLexer extends Lexer {
           (code >= 59 && code <= 64) || (code >= 33 && code <= 42) ||
           (code >= 47 && code <= 57) || (code >= 123 && code <= 153)) {
           this.elems.push(char);
-        } else if (char === '.') {
-          this.elems.push(char);
-          this.status = 5;
         } else {
-          this.ans.push(this.makeLexer('command', this.elems.join('')));
+          this.ans.push(this.makeLexer('filename', this.elems.join('')));
           return this.quit();
         }
         break;
       }
-      case 5: {
+      case 5:
+        if (char === '"') {
+          this.ans.push(this.makeLexer('string', this.elems.join('')));
+          return this.quit();
+        } else {
+          this.elems.push(char);
+        }
+        break;
+      case 6:
+        if (char === '"') {
+          this.ans.push(this.makeLexer('string', this.elems.join('')));
+          return this.quit();
+        } else {
+          this.elems.push(char);
+        }
+        break;
+      case 7:
+        if (char === '`') {
+          this.ans.push(this.makeLexer('string', this.elems.join('')));
+          return this.quit();
+        } else {
+          this.elems.push(char);
+        }
+        break;
+      case 8: {
+        if (char === 'EOF') {
+          this.ans.push(this.makeLexer('variable', this.elems.join('')));
+          return this.quit();
+        }
         const code = char.charCodeAt(0);
         if ((code >= 97 && code <= 122) ||
-          (code >= 59 && code <= 64) || (code >= 33 && code <= 42) ||
-          (code >= 47 && code <= 57) || (code >= 123 && code <= 153)) {
+          (code >= 65 && code <= 95) || (code >= 33 && code <= 42) ||
+          (code >= 45 && code <= 57) || (code >= 123 && code <= 153)) {
           this.elems.push(char);
         } else {
-          this.ans.push(this.makeLexer('filename', this.elems.join('')));
+          this.ans.push(this.makeLexer('variable', this.elems.join('')));
+          return this.quit();
+        }
+        break;
+      }
+      case 9: {
+        const code = char.charCodeAt(0);
+        if ((code >= 97 && code <= 122) || (code >= 65 && code <= 95)) {
+          this.elems.push(char);
+          this.status = 10;
+        } else {
+          this.ans.push(this.makeLexer('centerLine', '-'));
+        }
+        break;
+      }
+      case 10: {
+        if (char === 'EOF') {
+          this.ans.push(this.makeLexer('option', this.elems.join('')));
+          return this.quit();
+        }
+        const code = char.charCodeAt(0);
+        if ((code >= 97 && code <= 122) || (code >= 65 && code <= 95)) {
+          this.elems.push(char);
+        } else {
+          this.ans.push(this.makeLexer('option', this.elems.join('')));
+          return this.quit();
+        }
+        break;
+      }
+      case 11: {
+        if (char === 'EOF') {
+          this.ans.push(this.makeLexer('pathVariable', this.elems.join('')));
+          return this.quit();
+        }
+        const code = char.charCodeAt(0);
+        if ((code >= 97 && code <= 122) || (code >= 65 && code <= 95)) {
+          this.elems.push(char);
+        } else {
+          this.ans.push(this.makeLexer('pathVariable', this.elems.join('')));
           return this.quit();
         }
         break;
--- test/ShellLexer.test.js
+++ test/ShellLexer.test.js
@@ -7,5 +7,8 @@ describe('[class] ShellLexer', () => {
     const highLight = new HighLight();
     highLight.addLexer(ShellLexer);
     expect(JSON.stringify(highLight.parse('fulmination parse text.ctf'))).toMatch('[{\"type\":\"command\",\"elem\":\"fulmination\"},{\"type\":\" \"},{\"type\":\"command\",\"elem\":\"parse\"},{\"type\":\" \"},{\"type\":\"filename\",\"elem\":\"text.ctf\"}]');
+    expect(JSON.stringify(highLight.parse('echo "string"'))).toMatch('[{\"type\":\"command\",\"elem\":\"echo\"},{\"type\":\" \"},{\"type\":\"string\",\"elem\":\"\\\"string\"}]');
+    expect(JSON.stringify(highLight.parse('echo $ENV'))).toMatch('[{\"type\":\"command\",\"elem\":\"echo\"},{\"type\":\" \"},{\"type\":\"variable\",\"elem\":\"$ENV\"}]');
+    expect(JSON.stringify(highLight.parse('ls -a'))).toMatch('[{\"type\":\"command\",\"elem\":\"ls\"},{\"type\":\" \"},{\"type\":\"option\",\"elem\":\"-a\"}]');
   });
 });
